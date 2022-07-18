a64d304c10d9c2734c5a18106dd27e219f3704e9
koslowsky <79971751+koslowsky@users.noreply.github.com>
2022-07-18 20:10:28 +0300

    Starfleet

--- CrewWomam.java
+++ CrewWomam.java
@@ -1,3 +1,5 @@
+package il.ac.tau.cs.sw1.ex9.starfleet;
+
 public class CrewWoman extends myCrewMembersAbstract{
 	public CrewWoman(int age, int yearsInService, String name){
 		super(age, yearsInService, name);
