fd036cc1805645f485857d0f621e7c3692209c4b
koslowsky <79971751+koslowsky@users.noreply.github.com>
2022-07-18 19:53:08 +0300

    Create CylonRaider.java

---
+++ CylonRaider.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.Set;
+
+public class CylonRaider extends myFighterAbstract{
+
+	private int AnnualMaintenanceCost;
+
+	public CylonRaider(String name, int commissionYear, float maximalSpeed, Set<Cylon> crewMembers,
+			List<Weapon> weapons) {
+		super(name, commissionYear, maximalSpeed, crewMembers, weapons);
+
+		int TotalWeaponsCost=0;
+		for (Weapon weapon: weapons){
+			TotalWeaponsCost += weapon.getAnnualMaintenanceCost();
+		}
+		this.AnnualMaintenanceCost = 3500+(TotalWeaponsCost)+(500*crewMembers.size())+Math.round(1200*maximalSpeed);
+	}
+
+	public int getAnnualMaintenanceCost() {
+		return AnnualMaintenanceCost;
+	}
+	public String toString() {return getToStringFighterMutual();}
+}
