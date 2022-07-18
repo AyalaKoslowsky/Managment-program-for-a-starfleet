02fdff380209ef67fb15ae5b244499cc55e4c734
koslowsky <79971751+koslowsky@users.noreply.github.com>
2022-07-18 20:06:42 +0300

    Starfleet

---
+++ TransportShip.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.AnnotatedArrayType;
+import java.util.Set;
+
+public class TransportShip extends mySpaceshipAbstract{
+
+	private int CargoCapacity;
+	private int PassengerCapacity;
+	private int AnnualMaintenanceCost;
+
+
+	public TransportShip(String name, int commissionYear, float maximalSpeed, Set<CrewMember> crewMembers, int cargoCapacity, int passengerCapacity){
+		super (name, commissionYear, maximalSpeed, crewMembers);
+		this.CargoCapacity = cargoCapacity;
+		this.PassengerCapacity = passengerCapacity;
+		this.AnnualMaintenanceCost = 3000+(5*CargoCapacity)+(3*passengerCapacity);
+	}
+
+	public int getCargoCapacity() {return CargoCapacity;}
+	public int getPassengerCapacity() {return PassengerCapacity;}
+	@Override
+	public int getAnnualMaintenanceCost() {
+		return AnnualMaintenanceCost;
+	}
+
+	public String toString() {
+		return getToStringMutual() +System.lineSeparator()+ tab+"CargoCapacity=" + this.getCargoCapacity() +
+				System.lineSeparator()+tab+"PassengerCapacity=" + this.getPassengerCapacity();
+	}
+}
